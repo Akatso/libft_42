bf3c36c45bbffa9ac3d077af1f09fb4a87bc3898
Soraya Lepetit <slepetit@2D6.42angouleme.fr>
2022-04-14 01:38:40 +0200

    test00

---
+++ ft_memmove.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slepetit <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 11:05:40 by slepetit          #+#    #+#             */
+/*   Updated: 2022/03/31 10:20:40 by slepetit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*d;
+	char	*s;
+
+	d = (char *) dest;
+	s = (char *) src;
+	if (d < s)
+	{
+		ft_memcpy(d, s, n);
+		return (d);
+	}
+	while (n--)
+		*(d + n) = *(s + n);
+	return (d);
+}
